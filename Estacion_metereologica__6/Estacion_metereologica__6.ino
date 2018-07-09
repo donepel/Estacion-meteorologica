c23f8a8462b1817fc6751ef2993849a0558fb4ec
donepel <epel@epelbyte.com>
2018-07-09 14:33:34 -0300

    version 6

---
+++ Estacion_metereologica__6/Estacion_metereologica__6.ino
@@ -0,0 +1,61 @@
+/************************************************************
+ *                                                          *
+ *       MAIN Estacion Meteorologica                        * 
+ *       ver notas para conocer el versionado               *
+ *                                                          *
+ ************************************************************/ 
+
+#include <EtherCard.h> //https://github.com/jcw/ethercard
+#include <DHT.h>
+
+/*******************************************************
+ *Configuracion correspondiente al modulo ethernet     *
+ *******************************************************/
+static byte mymac[] = {0x74,0x69,0x69,0x2D,0x30,0x38};
+byte Ethernet::buffer[700];
+/******************** FIN ENC28J60 ********************/
+
+
+/**************************************************************
+ *Configuracion correspondiente al modulo DHT11               *
+ **************************************************************/
+#define DHTPIN 11 // Defino pin datos DHT
+#define DHTTYPE DHT11 //Defino modelo de sensor DHT11 o DHT22
+// Inicializo el sensor DHT
+DHT dht(DHTPIN, DHTTYPE);
+/************************* FIN DHT ***************************/
+
+
+//Definiciones varias, controles externos, variables, etc.
+const int Ventilador = 12; //pin donde se encuentra un forzador pra encender de forma remota
+const char* EstadoVent1="OFF";
+
+
+
+/*********************************************************** 
+ *               Funcion principal                         *
+ ***********************************************************/        
+void loop() 
+{
+  word len = ether.packetReceive();
+  word pos = ether.packetLoop(len);
+    
+  if(pos) 
+  {
+    
+    if(strstr((char *)Ethernet::buffer + pos, "GET /?status=ON") != 0) 
+    {
+      Serial.println("Comando ON recibido");
+      digitalWrite(Ventilador, HIGH);
+      EstadoVent1 = "ON";
+    }
+
+    if(strstr((char *)Ethernet::buffer + pos, "GET /?status=OFF") != 0) 
+    {
+      Serial.println("Comando OFF recibido");
+      digitalWrite(Ventilador, LOW);
+       EstadoVent1= "OFF";
+    }         
+    ether.httpServerReply(homePage()); // se envia página Web
+  }
+}
---
+++ Estacion_metereologica__6/Notas_de_version.ino
@@ -0,0 +1,40 @@
+/*****************************************************************
+**** Estacion METEOROLOGICA v6 (24-06-2018)
+Se corrige el nombre de meteorologica que estaba mal escrito
+Se continua ordenando y prolijando el codigo.
+
+**** Estacion meteorologica v5 (23-06-2018)
+Se elimina advertencia de C++ con la variable char* EstadoVent1
+Se divide el main principal en varios archivos
+Tamaño: 11838 bytes ROM / 1245 bytes RAM
+******************************************************************* 
+**** Estacion meteorologica v4 (23-06-2018) 
+Se corrige el tiempo anunciado en el sitio. en realidad es uptime
+Tamaño v3 pulida un poco 11368B ROM y 1357B RAM
+Se condigura con DHCP CLient para mejorar el Pulg & Play
+Tamaño: 12068 bytes ROM / 1309 bytes RAM
+
+Se pule el codigo actual con los cambios anteriores
+Tamaño: 11930 bytes ROM / 1261 bytes RAM
+*******************************************************************
+**** Estacion meteorologica v3 (23-06-2018)
+se agrega medicion de humedad
+se pule el codigo refente al dht11
+Se mejoran los comentarios de la funcion homePage()
+Se mejora un poco la pagina web
+Tamaño: 11482bites rom 1357 ram
+******************************************************************
+**** Estacion meteorologica v2 (23-06-2018)
+se configura el modulo ethernet enc28j60 con ip fija 
+se configura servidor web basico
+se configura DHT11
+sitio web muestra temperatura
+*******************************************************************
+**** Estacion meteorologica v1 (23-06-2018)
+se configura el modulo ethernet enc28j60 con ip fija
+*******************************************************************
+Prerequisitos:
+Libreria adafruit dht11-dht22 (instalable desde el gestor de librerias
+Libreria ethernetcard disponible en https://github.com/jcw/ethercard
+
+ *******************************************************************/
